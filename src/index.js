50999506c1757fc42143edd3165042b9ecac92c1
Sahar-SE <www.saharsaba.amiri123@gmail.com>
2022-06-15 20:49:25 +0430

    Fix Linters

--- src/index.js
+++ src/index.js
@@ -1,42 +1,43 @@
-//Createing a class for adding inputs
-require('./style.css')
-class BoardList {
-  constructor(name, score){
-      this.name = name;
-      this.score = score;
-    }
-}
+// Createing a class for adding inputs
+// require('./style.css');
 
-class DomDisplay{
-  static displayPerson(){
-    const player = [];
-    const players = player;
+// class BoardList {
+//   constructor(name, score) {
+//     this.name = name;
+//     this.score = score;
+//   }
+// }
 
-    players.forEach((person)=>{
-      DomDisplay.addPerson(person);
-    });
-  }
-  static addPerson(person){
-    const list = document.getElementById('playerList');
-    //create html elements for table
-    const row = document.createElement('tr');
-    row.innerHTML = `
-    <td>${person.name}</td>
-    <td${person.score}</td>
-    `;
-    list.appendChild(row);
-  }
-}
+// class DomDisplay {
+//   static displayPerson() {
+//     const player = [];
+//     const players = player;
 
-const username = document.getElementById('name');
-const scored = document.getElementById('num');
-const btn = document.getElementById('btn');
-const list = document.getElementById('list');
+//     players.forEach((person) => {
+//       DomDisplay.addPerson(person);
+//     });
+//   }
 
-btn.addEventListener('click', ()=>{
-    
-    list.innerHTML = username.value + num.value;
-});
+//   static addPerson(person) {
+//     const list = document.getElementById('playerList');
+//     // create html elements for table
+//     const row = document.createElement('tr');
+//     row.innerHTML = `
+//     <td>${person.name}</td>
+//     <td${person.score}</td>
+//     `;
+//     list.appendChild(row);
+//   }
+// }
 
-//event
-document.addEventListener('contentLoad', DomDisplay.displayPerson());
+// const username = document.getElementById('name');
+// const scored = document.getElementById('num');
+// const btn = document.getElementById('btn');
+// const list = document.getElementById('list');
+
+// btn.addEventListener('click', () => {
+//   list.innerHTML = username.value + num.value;
+// });
+
+// // event
+// document.addEventListener('contentLoad', DomDisplay.displayPerson());
